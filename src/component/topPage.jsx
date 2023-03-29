1fc696f1199add19b85385e34b093e3658bde94f
dharold1 <olamidedavid10@gmail.com>
2023-03-29 15:28:46 +0100

    added text to topPage

--- src/component/topPage.jsx
+++ src/component/topPage.jsx
@@ -3,7 +3,12 @@ import classes from '../styles/topPage.module.css'
 function TopPage() {
   return (
     <div className={classes.top}>
-      
+      <h1 className='topPage-mainText'>HELLO! I'm <span className={classes['orange-text']}>Ehiz</span></h1>
+      <h3 className='topPage-subText' style={{fontFamily: "roboto", fontSize:"40px"}}>I am passionate about UI/UX Design</h3>
+
+      <p className='topPage-subText'>Currently working at <span className={classes['orange-text']}>IRecharge Tech Innovation`Ltd</span></p>
+
+      <button className='bg-black text-gray-200 py-3 px-16 rounded hover:!bg-gray-900'> Read About My tech Journey</button>
     </div>
   )
 }
